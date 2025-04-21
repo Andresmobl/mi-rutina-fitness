68d4c059fea0b939c99baa146b8791b98d64afd5
Andresmobl <andresmorenoblanc1996@gmail.com>
2025-04-21 14:50:58 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mi Rutina Fitness',
+  webDir: 'www'
+};
+
+export default config;
